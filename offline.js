e682cd6cce9b2750543d5f1bf03e79fb388be2f7
00Rez <1608196+00Rez@users.noreply.github.com>
2022-02-19 20:23:49 +0000

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1645298648,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tile-sheet0.png",
+		"images/camerapoint-sheet0.png",
+		"images/bomb-sheet0.png",
+		"images/energy-sheet0.png",
+		"images/energy-sheet1.png",
+		"images/controller-sheet0.png",
+		"images/score.png",
+		"images/stackposition-sheet0.png",
+		"images/stack-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/shadow-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
